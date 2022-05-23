1ecc7b0534680c3d655b0c50eca5975269a1b4d4
Aloisio de Araujo Costa Magalhaes <educacao1ouro@gmail.com>
2022-05-23 09:49:15 -0300

    resolvi a questao 4

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -20,7 +20,7 @@ function retornaArrayOrdenado(array) {
 
 // EXERCÍCIO 04
 function retornaNumerosPares(array) {
-  //return array.filter(array % 2===0);  
+  return array.filter(array % 2===0);  
 }
 
 // EXERCÍCIO 05
