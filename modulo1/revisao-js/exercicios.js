75db7b5de27d44dd6f59f42d36cbe9470746b3ad
Aloisio de Araujo Costa Magalhaes <educacao1ouro@gmail.com>
2022-05-23 11:38:03 -0300

    resolvi a questao 11

--- modulo1/revisao-js/exercicios.js
+++ modulo1/revisao-js/exercicios.js
@@ -75,7 +75,8 @@ function retornaSegundoMaiorESegundoMenor(array) {
 
 // EXERCÍCIO 11
 function retornaChamadaDeFilme(filme) {
-   
+// saída
+return `Venha assistir ao filme ${filme.nome}, de ${filme.ano}, dirigido por ${filme.diretor} e estrelado por ${filme.atores[0]}, ${filme.atores[1]}, ${filme.atores[2]}, ${filme.atores[3]}.`;
 }
 
 // EXERCÍCIO 12
