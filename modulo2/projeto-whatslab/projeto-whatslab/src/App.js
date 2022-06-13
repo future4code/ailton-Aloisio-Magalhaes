14712672df1b6d8314c4fc7b7a1507ef513d7cc7
Aloisio de Araujo Costa Magalhaes <educacao1ouro@gmail.com>
2022-06-13 16:07:38 -0300

    app funcionando na web

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,25 +1,87 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const AppContainer = styled.div`
+  border: 1px solid black;
+  height: 100vh;
+  box-sizing: border-box;
+  width: 600px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+`
+
+const MessagesContainer = styled.div`
+  flex-grow: 1;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+`
+
+const InputsContainer = styled.div`
+  display: flex;
+`
+
+const NameInput = styled.input`
+  width: 100px;
+`
+
+const MessageInput = styled.input`
+  flex-grow: 1;
+`
+
+class App extends React.Component {
+  state = {
+    messages: [],
+    userValue: '',
+    messageValue: ''
+  }
+
+  onChangeUserValue = (event) => {
+    this.setState({userValue: event.target.value})
+  }
+
+  onChangeMessageValue = (event) => {
+    this.setState({messageValue: event.target.value})
+  }
+
+  sendMessage = () => {
+    const newMessage = {
+      user: this.state.userValue,
+      text: this.state.messageValue
+    }
+
+    const newMessagesArray = [newMessage, ...this.state.messages]
+
+    this.setState({messages: newMessagesArray, messageValue: ''})
+  }
+
+  render() {
+    return (
+      <AppContainer>
+        <MessagesContainer>
+          {this.state.messages.map((message, index) => {
+            return <p key={index}>
+              <strong>{message.user}</strong>: {message.text}
+            </p>
+          })}
+        </MessagesContainer>
+        <InputsContainer>
+          <NameInput
+            onChange={this.onChangeUserValue} 
+            value={this.state.userValue} 
+            placeholder={'Nome'}
+          />
+          <MessageInput
+            onChange={this.onChangeMessageValue} 
+            value={this.state.messageValue} 
+            placeholder={'Mensagem'}
+          />
+          <button onClick={this.sendMessage}>Enviar</button>
+        </InputsContainer>
+      </AppContainer>
+    );
+  }
 }
 
 export default App;
