213a439e24c46b81b38cc1472d792e3452fbbd0d
Aloisio de Araujo Costa Magalhaes <educacao1ouro@gmail.com>
2022-06-14 15:42:28 -0300

    feature do boltao select alterando entre as opcoes listadas

--- modulo2/ciclo-vida-todo/lista-tarefas/src/App.js
+++ modulo2/ciclo-vida-todo/lista-tarefas/src/App.js
@@ -2,21 +2,29 @@ import React from 'react'
 import styled from 'styled-components'
 import './App.css'
 
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  box-sizing: border-box;
+  margin: auto;
+  gap: 10px;
+`
+
 const TarefaList = styled.ul`
   padding: 0;
   width: 200px;
 `
-
+/* TarefaList > listaFiltrada
+key={tarefaId} 
+                tarefa={tarefa} 
+                RemoverTarefa=
+*/
 const Tarefa = styled.li`
   text-align: left;
   text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
 `
-
-const InputsContainer = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  gap: 10px;
-`
+//const RemoverTarefa = styled.li``
+//const EditarTarefa = styled.li``
 
 class App extends React.Component {
     state = {
@@ -27,7 +35,7 @@ class App extends React.Component {
                 texto: 'Texto da segunda tarefa',
                 completa: true}],
       inputValue: '',
-      filtro: 'pendentes'
+      filtro: 'nenhum'
     }
 
   componentDidUpdate() {
@@ -39,19 +47,38 @@ class App extends React.Component {
   };
 
   onChangeInput = (event) => {
-
+    this.setState({inputValue: event.target.value})
   }
 
   criaTarefa = () => {
-
+    console.log('add tarefa', this.state.inputValue)
+    const novaTarefa = {id: Date.now(),
+                        texto: this.state.inputValue,
+                        completa: false}
+    const novaListaDeTarefas = [novaTarefa, ...this.state.tarefas]
+    this.setState({tarefas: novaListaDeTarefas})
   }
-
-  selectTarefa = (id) => {
-
+  
+  apagarTarefa = (tarefaId) => {
+    console.log('apagar tarefa', tarefaId)
+    const novaListaDeTarefas = this.state.tarefas.filter((tarefa)=>{return tarefaId !== tarefa.id})
+    this.setState({tarefas: novaListaDeTarefas})
+  }
+  
+  editarTarefa = (tarefaId) => {
+    //mudar o state.texto ja existente
+    const novaListaDeTarefas = this.state.tarefas.map(
+      (tarefa)=>{return tarefa = (tarefa.id === tarefaId) ? (tarefa = {...tarefa, completa: !tarefa.completa}): false   
+      })
+      this.setState({tarefas: novaListaDeTarefas})
+  }
+  
+  selectTarefa = (tarefaId) => {
+    //mudar o texto no state.completo ja existente
   }
 
   onChangeFilter = (event) => {
-
+      this.setState({filtro: event.target.value})
   }
 
   render() {
@@ -70,7 +97,7 @@ class App extends React.Component {
       <div className="App">
         <h1>Lista de tarefas</h1>
         <InputsContainer>
-          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <input type="text" value={this.state.inputValue} onChange={this.onChangeInput}/>
           <button onClick={this.criaTarefa}>Adicionar</button>
         </InputsContainer>
         <br/>
@@ -86,12 +113,12 @@ class App extends React.Component {
         <TarefaList>
           {listaFiltrada.map(tarefa => {
             return (
-              <Tarefa
-                completa={tarefa.completa}
-                onClick={() => this.selectTarefa(tarefa.id)}
-              >
-                {tarefa.texto}
+              <Tarefa  
+                completa={tarefa.completa} 
+                onClick={() => this.selectTarefa(tarefa.id)}>
+                                                              {tarefa.texto}
               </Tarefa>
+              //<RemoverTarefa></RemoverTarefa>
             )
           })}
         </TarefaList>
