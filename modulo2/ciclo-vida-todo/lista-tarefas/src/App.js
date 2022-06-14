7d20bfee40a7131dcc8f989858a69171fadcc0e3
Aloisio de Araujo Costa Magalhaes <educacao1ouro@gmail.com>
2022-06-14 14:16:14 -0300

    app lista-tarefas criado localmente do zero

---
+++ modulo2/ciclo-vida-todo/lista-tarefas/src/App.js
@@ -0,0 +1,104 @@
+import React from 'react'
+import styled from 'styled-components'
+import './App.css'
+
+const TarefaList = styled.ul`
+  padding: 0;
+  width: 200px;
+`
+
+const Tarefa = styled.li`
+  text-align: left;
+  text-decoration: ${({completa}) => (completa ? 'line-through' : 'none')};
+`
+
+const InputsContainer = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  gap: 10px;
+`
+
+class App extends React.Component {
+    state = {
+      tarefas: [{id: Date.now(),
+                texto: 'Texto da primeira tarefa',
+                completa: false},
+               {id: Date.now(),
+                texto: 'Texto da segunda tarefa',
+                completa: true}],
+      inputValue: '',
+      filtro: 'pendentes'
+    }
+
+  componentDidUpdate() {
+
+  };
+
+  componentDidMount() {
+
+  };
+
+  onChangeInput = (event) => {
+
+  }
+
+  criaTarefa = () => {
+
+  }
+
+  selectTarefa = (id) => {
+
+  }
+
+  onChangeFilter = (event) => {
+
+  }
+
+  render() {
+    const listaFiltrada = this.state.tarefas.filter(tarefa => {
+      switch (this.state.filtro) {
+        case 'pendentes':
+          return !tarefa.completa
+        case 'completas':
+          return tarefa.completa
+        default:
+          return true
+      }
+    })
+
+    return (
+      <div className="App">
+        <h1>Lista de tarefas</h1>
+        <InputsContainer>
+          <input value={this.state.inputValue} onChange={this.onChangeInput}/>
+          <button onClick={this.criaTarefa}>Adicionar</button>
+        </InputsContainer>
+        <br/>
+
+        <InputsContainer>
+          <label>Filtro</label>
+          <select value={this.state.filter} onChange={this.onChangeFilter}>
+            <option value="">Nenhum</option>
+            <option value="pendentes">Pendentes</option>
+            <option value="completas">Completas</option>
+          </select>
+        </InputsContainer>
+        <TarefaList>
+          {listaFiltrada.map(tarefa => {
+            return (
+              <Tarefa
+                completa={tarefa.completa}
+                onClick={() => this.selectTarefa(tarefa.id)}
+              >
+                {tarefa.texto}
+              </Tarefa>
+            )
+          })}
+        </TarefaList>
+      </div>
+    )
+  }
+}
+
+export default App
+
